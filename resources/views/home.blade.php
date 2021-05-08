c3a334476c95baec6189ac46959f6c76b6a2819d
lincnz <lincnz@gmail.com>
2021-05-09 11:45:33 +1200

    re-up commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-3">
+			<div class="rounded-circle m-5" style="width:5em; height:5em; background-color:green;">
+			</div>
+		</div>
+		<div class="col-9">
+			<div><h1>freecodecamp</h1></div>
+			<div class="d-flex">
+				<div class="pr-5" ><strong>153 </strong>posts</div>
+				<div class="pr-5"><strong>23k </strong>followers</div>
+				<div class="pr-5"><strong>212 </strong>following</div>
+				
+			</div>
+			<div class="pt-4 font-weight-bold">freecodecamp.org</div>
+			<div>bla bla bla</div>
+			<div><a href="/">bla bla bla.com</a></div>
+			</div>
+		</div>
+	<div class="row pt-4">
+		<div class="col-4">
+			<img class="w-100" src="instagram.fchc1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/183578102_292774165720338_8809966714281948877_n.jpg?tp=1&_nc_ht=instagram.fchc1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=UmI0uxoV95AAX9kLM32&edm=AIQHJ4wBAAAA&ccb=7-4&oh=6dc7f7b9b28f7fbff604979c209c2c10&oe=60BBA24F&_nc_sid=7b02f1">
+			<img class="w-100" src="instagram.fchc1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/183578102_292774165720338_8809966714281948877_n.jpg?tp=1&_nc_ht=instagram.fchc1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=UmI0uxoV95AAX9kLM32&edm=AIQHJ4wBAAAA&ccb=7-4&oh=6dc7f7b9b28f7fbff604979c209c2c10&oe=60BBA24F&_nc_sid=7b02f1">
+			<img class="w-100" src="instagram.fchc1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s750x750/183578102_292774165720338_8809966714281948877_n.jpg?tp=1&_nc_ht=instagram.fchc1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=UmI0uxoV95AAX9kLM32&edm=AIQHJ4wBAAAA&ccb=7-4&oh=6dc7f7b9b28f7fbff604979c209c2c10&oe=60BBA24F&_nc_sid=7b02f1">			
+		</div>
+	</div>
+</div>
+@endsection
