28af58ad572196332f2933dfa2cf923241663d0a
lincnz <lincnz@gmail.com>
2021-05-10 16:05:56 +1200

    te anau commit 2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,7 +40,7 @@
                         </div>
 						
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                            <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
 
                             <div class="col-md-6">
                                 <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username">
